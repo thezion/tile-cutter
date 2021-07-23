25d1f2117a6a2e549d4f9ec5c4f62046e715e2f7
John Long <kiino@qq.com>
2021-07-22 20:44:32 -0600

    zoom + 3

--- delete.bat
+++ delete.bat
@@ -1,23 +1,23 @@
 :: 9025 = 5090个文件
 FOR /L %%i IN (-48,1,-24) DO (
-  del lv8\8\tile-%%i_*.jpg
+  del lv11\11\tile-%%i_*.jpg
 )
 FOR /L %%i IN (21,1,47) DO (
-  del lv8\8\tile-*_%%i.jpg
+  del lv11\11\tile-*_%%i.jpg
 )
 
 :: 个文件
 FOR /L %%i IN (-24,1,-13) DO (
-  del lv7\7\tile-%%i_*.jpg
+  del lv10\10\tile-%%i_*.jpg
 )
 FOR /L %%i IN (11,1,24) DO (
-  del lv7\7\tile-*_%%i.jpg
+  del lv10\10\tile-*_%%i.jpg
 )
 
 :: 个文件
 FOR /L %%i IN (-12,1,-7) DO (
-  del lv7\6\tile-%%i_*.jpg
+  del lv9\9\tile-%%i_*.jpg
 )
 FOR /L %%i IN (5,1,12) DO (
-  del lv7\6\tile-*_%%i.jpg
+  del lv9\9\tile-*_%%i.jpg
 )
